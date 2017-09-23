df7452a7d06c78ad69aa87d993b36f72e46ab995
demonic-bling <gauthamvinod@outlook.com>
2017-09-23 08:47:54 +0530

    First Commit

---
+++ app/src/main/java/random/sampleapp/MainActivity.java
@@ -0,0 +1,28 @@
+package random.sampleapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
